c138f93a02ab3782d5e8a74aebb0529d501ee67d
Edward Kim <edwardjkim@Edwards-MacBook-Air-3.local>
2016-09-17 19:43:16 -0700

    create seed file and add pry to gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-user1 = User.create({name: 'Max', grade: 2, img_url: '', teacher?: false})
-
-user2 = User.create({name: 'Ruby', grade: 3, img_url: '', teacher?: false})
-user3 = User.create({name: 'Kelvin', grade: -1, img_url: '', teacher?: true})
+25.times do
+  User.create(name: Faker::Internet.user_name, grade: rand(1..8) , img_url: "https://robohash.org/#{"abcdefghijk"[rand(0..10)]}", teacher?: false)
+end
 
 interest1 = Interest.create({name: 'Drones'})
 interest2 = Interest.create({name: 'Cooking'})
-interest3 = Interest.create({name: 'Lollipop Chainsaw'})
+interest3 = Interest.create({name: 'Art'})
+interest4 = Interest.create({name: 'Basketball'})
+interest5 = Interest.create({name: 'Football'})
+
+array = [Interest.find(1), Interest.find(2), Interest.find(3), Interest.find(4), Interest.find(5)]
+interest1 = array.sample
+interest2 = array.sample
+
+User.all.each do |user|
+  interest1 = array.sample
+  interest2 = array.sample
+  until interest1 != interest2 do
+    interest1 = array.sample
+    interest2 = array.sample
+  end
+  UserInterest.create(user_id: user.id, interest_id: interest1.id)
+  UserInterest.create(user_id: user.id, interest_id: interest2.id)
+end
+
 
 # ui1 = UserInterests.create({user_id: 1, interest_id: 2})
 # user2.interest =
