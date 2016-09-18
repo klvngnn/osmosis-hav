ca5292f01ec7576ba50aa2fc6e37acb376df901a
Edward Kim <edwardjkim@Edwards-MacBook-Air-3.local>
2016-09-17 18:01:04 -0700

    fix user_interests

--- db/migrate/20160917230651_create_user_interests.rb
+++ db/migrate/20160917230651_create_user_interests.rb
@@ -1,7 +1,8 @@
 class CreateUserInterests < ActiveRecord::Migration[5.0]
   def change
     create_table :user_interests do |t|
-
+        t.references :user
+        t.references :interest
       t.timestamps
     end
   end
