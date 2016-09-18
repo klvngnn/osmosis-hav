43c7882e496ce1bb1c63d043bd94c03cbc7ddc6b
klvngnn <klvngnn@gmail.com>
2016-09-18 09:17:56 -0700

    added matching view

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -5,6 +5,11 @@ class MatchingController < ApplicationController
   	@user = User.find(1)
 		@bully = User.find(2)
 
+		@match1 = User.find(3)
+		@match2 = User.find(4)
+		@match3 = User.find(5)
+		@match4 = User.find(6)
+
 		render :new
 	end
 
